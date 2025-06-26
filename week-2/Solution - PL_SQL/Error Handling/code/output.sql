95f55e800abc0583bc8fde9382285da3e279af26
omrai-github <omrai352005@gmail.com>
2025-06-26 10:08:26 +0530

    Error Handling Solution

---
+++ week-2/Solution - PL_SQL/Error Handling/code/output.sql
@@ -0,0 +1,39 @@
+-- SELECT * FROM Error_Log ORDER BY LogTime DESC;
+-- BEGIN
+--     SafeTransferFunds(p_from_acct => 1, p_to_acct => 2, p_amount => 3000);
+-- END;
+
+-- SELECT * FROM Error_Log ORDER BY LogTime DESC;
+
+
+-- BEGIN
+--     SafeTransferFunds(p_from_acct => 1, p_to_acct => 2, p_amount => 50);
+-- END;
+
+-- SELECT AccountID, Balance FROM Accounts WHERE AccountID IN (1, 2);
+
+-- BEGIN
+--     UpdateSalary(4, 10);  
+-- END;
+-- SELECT * FROM Error_Log ORDER BY LogTime DESC;
+
+--    BEGIN 
+    --   updateSalary(1 , )
+
+--   SELECT * from Employees; 
+
+--  BEGIN
+--     UpdateSalary(1, 20 );  
+-- END; 
+--  SELECT * from Employees;
+
+-- BEGIN
+--     AddNewCustomer(3, 'Divyanshu', TO_DATE('1990-01-01', 'YYYY-MM-DD'), 2500);
+-- END;
+--   SELECT * FROM Error_Log ORDER BY LogTime DESC;
+   
+ BEGIN
+    AddNewCustomer(6, 'JOHn Due', TO_DATE('1991-02-01', 'YYYY-MM-DD'), 2500);
+END;
+
+--    Select * from CUSTOMERS where CUSTOMERID = 6 ; 
---
+++ week-2/Solution - PL_SQL/Error Handling/output/output.md
@@ -0,0 +1,34 @@
+# Scenario 1 outputs
+
+**Error**
+![alt text](Img/image.png)
+
+**working**
+
+![alt text](Img/image-1.png)
+
+# Scenario 2 outputs 
+
+**Error**
+
+![alt text](Img/image-2.png)
+
+**working**
+
+previous salary of employee 1 is 60000
+
+![alt text](Img/image-3.png)
+
+updated salary of employee 1 is 72000 i.e. 20 percent rase 
+
+![alt text](Img/image-4.png)
+
+# Scenario 3 Outputs 
+
+**Error** 
+
+![alt text](Img/image-5.png)
+
+**working** 
+
+![alt text](Img/image-6.png)
